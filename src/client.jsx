04a0c00293bd16a8b8f1710ce43e61a4723cf358
chriszarate <chris@zarate.org>
2017-08-22 15:36:42 -0400

    Provide reduxRootSelector so that initial state is selected.

--- src/client.jsx
+++ src/client.jsx
@@ -4,6 +4,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo';
 import App from './App';
 
 const apolloClient = new ApolloClient({
+  reduxRootSelector: client => client.apollo,
   initialState: window.__APOLLO_STATE__,
   networkInterface: createNetworkInterface({
     uri: 'http://localhost:3000/graph',
